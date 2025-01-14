5a6e56f71cd6dc1e42db64ab6c3a4384d2e41fa3
kwachkar <kwachkar@grepruby.io>
2025-01-14 22:44:27 +0530

    landing section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
+import LandingSection from './components/landingSection'
 
 function App() {
 
   return (
     <>
      <Header/>
+     <LandingSection/>
     </>
   )
 }
